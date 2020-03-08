824e30d32effcd730a5efea37d9d371a30207466
Sanyog Gupta <sanyoggupta42@gmail.com>
2020-03-09 00:42:20 +0530

    sample data

---
+++ Samples/Image/t1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	if(A==9) {
+		while(A==9) {
+			A=A+1;
+		}
+		B=10;
+		do {
+			B=B+1;
+		} while(B<12);
+	}
+	return 0;
+}
---
+++ Samples/Image/t1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	if(A==9) {
+		while(A==9) {
+			A=A+1;
+		}
+		B=10;
+		do {
+			B=B+1;
+		} while(B<12);
+	}
+	return 0;
+}
